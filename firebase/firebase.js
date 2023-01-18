cdcca54e7f69904c81b1e57dc69927d88c71bc54
Ibranista <ibranista88@gmail.com>
2023-01-18 11:33:14 +0100

    initialized react, next and firebase app.

---
+++ firebase/firebase.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMpRJNlTTQlvmtjFtGLfym3F7CO0ToFaE",
+  authDomain: "expense-tracker-98856.firebaseapp.com",
+  projectId: "expense-tracker-98856",
+  storageBucket: "expense-tracker-98856.appspot.com",
+  messagingSenderId: "284595854598",
+  appId: "1:284595854598:web:9408caa0e85f9e2b115287",
+  measurementId: "G-7E7NKQZE4G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
