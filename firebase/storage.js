674fd314897bce4c86d1ead0972c23233eec38ad
Ibranista <ibranista88@gmail.com>
2023-01-25 13:28:40 +0100

    upload image using firebase storage.

--- firebase/storage.js
+++ firebase/storage.js
@@ -15,6 +15,21 @@
  * limitations under the License.
  */
 
-import { format } from 'date-fns';
-import { deleteObject, getDownloadURL as getStorageDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { storage } from './firebase';
+import { format } from "date-fns";
+import {
+  deleteObject,
+  getDownloadURL as getStorageDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { storage } from "./firebase";
+
+const BUCKET_URL = "gs://expense-tracker-98856.appspot.com";
+
+export async function uploadImage(image, uID) {
+  const formattedDate = format(new Date(), "yyyy-MM-dd-HH-mm-ss");
+  const bucket = `${BUCKET_URL}/${uID}/${formattedDate}.jpg`;
+  const storageRef = ref(storage, bucket);
+  await uploadBytes(storageRef, image);
+  return bucket;
+}
